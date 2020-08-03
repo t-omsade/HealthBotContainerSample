21c28b541864ad09b2625d4d97c4ecf6e765320a
t-omsade <68709230+t-omsade@users.noreply.github.com>
2020-08-03 19:11:35 +0300

    make covid19_general_form the default scenario

--- public/index.js
+++ public/index.js
@@ -105,15 +105,10 @@ function initBotConversation() {
                             jsonWebToken: jsonWebToken,
 
                             // Use the following activity to proactively invoke a bot scenario
-                            /*
                             triggeredScenario: {
-                                trigger: "{scenario_id}",
-                                args: {
-                                    myVar1: "{custom_arg_1}",
-                                    myVar2: "{custom_arg_2}"
-                                }
+                                trigger: "covid19_general_form",
+                                args: { }
                             }
-                            */
                         }
                     }
                 }
