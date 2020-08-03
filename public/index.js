26699e92f3fa41ed8a8c78d1c3f5f58ef58d890e
t-omsade <68709230+t-omsade@users.noreply.github.com>
2020-08-03 19:16:07 +0300

    Changed the bot icon

--- public/index.js
+++ public/index.js
@@ -81,7 +81,7 @@ function initBotConversation() {
         domain: domain
     });
     const styleOptions = {
-        botAvatarImage: 'https://docs.microsoft.com/en-us/azure/bot-service/v4sdk/media/logo_bot.svg?view=azure-bot-service-4.0',
+        botAvatarImage: 'EpiHippoIcon.png',
         // botAvatarInitials: '',
         // userAvatarImage: '',
         hideSendBox: false, /* set to true to hide the send box from the view */
